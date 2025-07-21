ee1513bff1fb72580c7c81adf90d3bedf8779a09
Brylle <luckybrylleramos@gmail.com>
2025-07-21 04:58:38 +0400

    blue-banner added on homepage

--- front-page.php
+++ front-page.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 <main>
-    <?php get_template_part('template-parts/hero'); ?>
+    <?php get_template_part('template-parts/homepage/hero'); ?>
+    <?php get_template_part('template-parts/homepage/blue-banner'); ?>
 
 </main>
 <?php get_footer(); ?>
