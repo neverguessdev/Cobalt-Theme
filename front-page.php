6403adfa18ecf9d3861565cc0e37093366bb0afd
Brylle <luckybrylleramos@gmail.com>
2025-07-24 20:09:58 +0400

    cobalt-difference added on homepage

--- front-page.php
+++ front-page.php
@@ -3,6 +3,7 @@
     <?php get_template_part('template-parts/homepage/hero'); ?>
     <?php get_template_part('template-parts/homepage/blue-banner'); ?>
     <?php get_template_part('template-parts/homepage/leadership-search'); ?>
+    <?php get_template_part('template-parts/homepage/cobalt-difference'); ?>
 
 </main>
 <?php get_footer(); ?>
---
+++ template-parts/homepage/cobalt-difference.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Template part for displaying the Cobalt Difference section
+ *
+ * @package Cobalt Theme
+ */
+?>
+<section class="cobalt-difference">
+    <div class="section-container">
+        <span class="section-pretitle-line">
+            <h2 class="section-pretitle">The Cobalt Difference</h2>
+        </span>
+        <div class="section-content">
+            <div class="left-side">
+                <h2 class="section-title">
+                    Your <span class="purple-text">Next Chapter</span> Deserves The Leader Who Can <span
+                            class="purple-text">Write It</span>
+                </h2>
+                <p class="section-subtext">Bright futures require visionary leadership. We go beyond traditional
+                    recruitment to combine deep market research, structured assessments, and culture-first
+                    decision-making. So you get your choice of game-changing candidates who can inspire, align, and
+                    shape the future of your business.
+                </p>
+                <img src="/wp-content/uploads/2025/07/globe-images-min.png" alt="Cobalt Difference Image" class="cobalt-difference-image">
+            </div>
+
+            <div class="multi-color-cards">
+                <div class="color-cards  blue">
+                    <div class="circle-log number-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                            <path d="M15.6674 17.4999H5.13242C3.00909 17.4999 1.65576 15.225 2.68242 13.3583L5.40076 8.4116L7.94409 3.77993C9.00576 1.85493 11.7824 1.85493 12.8441 3.77993L15.3991 8.4116L16.6241 10.6399L18.1174 13.3583C19.1441 15.225 17.7908 17.4999 15.6674 17.4999Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M25.6667 18.0833C25.6667 22.2717 22.2717 25.6667 18.0833 25.6667C13.895 25.6667 10.5 22.2717 10.5 18.0833C10.5 17.885 10.5117 17.6983 10.5233 17.5H15.6683C17.7917 17.5 19.145 15.225 18.1183 13.3583L16.625 10.64C17.0917 10.5467 17.5817 10.5 18.0833 10.5C22.2717 10.5 25.6667 13.895 25.6667 18.0833Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <h3 class="card-title">Real Search. Not Recycled Shortlists.</h3>
+                    <p class="card-text">We start every search from scratch, mapping the global market to find standout leaders. Not just those who are available, but those who are exceptional.</p>
+                </div>
+                <div class="color-cards  purple">
+                    <div class="circle-log number-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                            <path d="M10.5007 25.6666H17.5007C23.334 25.6666 25.6673 23.3333 25.6673 17.4999V10.4999C25.6673 4.66659 23.334 2.33325 17.5007 2.33325H10.5007C4.66732 2.33325 2.33398 4.66659 2.33398 10.4999V17.4999C2.33398 23.3333 4.66732 25.6666 10.5007 25.6666Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M18.0833 21.5834C19.3667 21.5834 20.4167 20.5334 20.4167 19.2501V8.75008C20.4167 7.46675 19.3667 6.41675 18.0833 6.41675C16.8 6.41675 15.75 7.46675 15.75 8.75008V19.2501C15.75 20.5334 16.7883 21.5834 18.0833 21.5834Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M9.91732 21.5833C11.2007 21.5833 12.2507 20.5333 12.2507 19.2499V15.1666C12.2507 13.8833 11.2007 12.8333 9.91732 12.8333C8.63398 12.8333 7.58398 13.8833 7.58398 15.1666V19.2499C7.58398 20.5333 8.62232 21.5833 9.91732 21.5833Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <h3 class="card-title">Aligned on Culture. Selected for Impact.</h3>
+                    <p class="card-text">A-player leadership isn’t just about skills and experience. We go further to find leaders who align with your values, inspire your people, and drive business performance.</p>
+                </div>
+                <div class="color-cards  green">
+                    <div class="circle-log number-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                            <path d="M15.6674 17.4999H5.13242C3.00909 17.4999 1.65576 15.225 2.68242 13.3583L5.40076 8.4116L7.94409 3.77993C9.00576 1.85493 11.7824 1.85493 12.8441 3.77993L15.3991 8.4116L16.6241 10.6399L18.1174 13.3583C19.1441 15.225 17.7908 17.4999 15.6674 17.4999Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M25.6667 18.0833C25.6667 22.2717 22.2717 25.6667 18.0833 25.6667C13.895 25.6667 10.5 22.2717 10.5 18.0833C10.5 17.885 10.5117 17.6983 10.5233 17.5H15.6683C17.7917 17.5 19.145 15.225 18.1183 13.3583L16.625 10.64C17.0917 10.5467 17.5817 10.5 18.0833 10.5C22.2717 10.5 25.6667 13.895 25.6667 18.0833Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <h3 class="card-title">Our process is your advantage</h3>
+                    <p class="card-text">Every candidate is proven through two rounds of in-depth technical and cultural interviews. So you only meet A-players who fit, lead, and deliver.</p>
+                </div>
+                <div class="color-cards  blue">
+                    <div class="circle-log number-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+                            <path d="M12.834 23.3333C18.633 23.3333 23.334 18.6322 23.334 12.8333C23.334 7.03426 18.633 2.33325 12.834 2.33325C7.03499 2.33325 2.33398 7.03426 2.33398 12.8333C2.33398 18.6322 7.03499 23.3333 12.834 23.3333Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M22.0858 24.138C22.7041 26.0047 24.1158 26.1914 25.2008 24.558C26.1925 23.0647 25.5391 21.8397 23.7425 21.8397C22.4125 21.828 21.6658 22.8664 22.0858 24.138Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </div>
+                    <h3 class="card-title">Small, Agile, and Built Around You</h3>
+                    <p class="card-text">You will work with the same senior team throughout - adaptable, hands-on, and invested in your success.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
